4588bc191870a4ac753d14bc45dd7e0ad735358f
windows10 <2380253499@qq.com>
2020-02-01 03:11:59 +0800

    file

---
+++ PathAnim/src/main/java/com/github/pathanim/PathAnimatorEvaluator.java
@@ -0,0 +1,14 @@
+package com.github.pathanim;
+
+import android.animation.TypeEvaluator;
+
+/***
+ *   created by zhongrui on 2020/2/1
+ */
+public class PathAnimatorEvaluator implements TypeEvaluator<PathPoint> {
+    @Override
+    public PathPoint evaluate(float fraction, PathPoint startValue, PathPoint endValue) {
+        PathPoint pathPoint=new PathPoint();
+        return pathPoint;
+    }
+}
---
+++ PathAnim/src/main/java/com/github/pathanim/PathPoint.java
@@ -0,0 +1,7 @@
+package com.github.pathanim;
+
+/***
+ *   created by zhongrui on 2020/2/1
+ */
+public class PathPoint {
+}
